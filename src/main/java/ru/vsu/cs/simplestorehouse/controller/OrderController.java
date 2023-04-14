f8fe244e2b07461a724bea3fb6b85e0abab447dc
Cappuchinka <volchenko.coffee@gmail.com>
2023-04-15 01:31:04 +0300

    Edit OrderService. Make OrderController.

--- src/main/java/ru/vsu/cs/simplestorehouse/controller/OrderController.java
+++ src/main/java/ru/vsu/cs/simplestorehouse/controller/OrderController.java
@@ -1,9 +1,10 @@
 package ru.vsu.cs.simplestorehouse.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import ru.vsu.cs.simplestorehouse.mapper.OrderMapper;
+import org.springframework.web.bind.annotation.*;
+import ru.vsu.cs.simplestorehouse.dto.OrderDto;
 import ru.vsu.cs.simplestorehouse.service.OrderService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/order")
@@ -13,5 +14,30 @@ public class OrderController {
 
     public OrderController(OrderService orderService) {
         this.orderService = orderService;
+    }
+
+    @GetMapping("/orders")
+    public List<OrderDto> getAll() {
+        return orderService.getOrders();
+    }
+
+    @PostMapping("/order/id")
+    public OrderDto getOrderById(@RequestBody Integer id) {
+        return orderService.getOrder(id);
+    }
+
+    @PostMapping("order/new")
+    public void addOrder(@RequestBody OrderDto orderDto) {
+        orderService.addOrder(orderDto);
+    }
+
+    @DeleteMapping("/clear")
+    public void clear() {
+        orderService.clear();
+    }
+
+    @DeleteMapping("order/delete")
+    public void delete(@RequestBody Integer id) {
+        orderService.delete(id);
     }
 }
--- src/main/java/ru/vsu/cs/simplestorehouse/service/OrderService.java
+++ src/main/java/ru/vsu/cs/simplestorehouse/service/OrderService.java
@@ -3,28 +3,33 @@ package ru.vsu.cs.simplestorehouse.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.vsu.cs.simplestorehouse.dto.OrderDto;
 import ru.vsu.cs.simplestorehouse.entity.Order;
+import ru.vsu.cs.simplestorehouse.mapper.OrderMapper;
 import ru.vsu.cs.simplestorehouse.repository.OrderRepository;
 import ru.vsu.cs.simplestorehouse.utils.exceptions.OrderNotFoundException;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final OrderMapper orderMapper;
 
-    public OrderService(OrderRepository orderRepository) {
+    public OrderService(OrderRepository orderRepository, OrderMapper orderMapper) {
         this.orderRepository = orderRepository;
+        this.orderMapper = orderMapper;
     }
 
-    public List<Order> getOrderProducts() {
-        return orderRepository.findAll();
+    public List<OrderDto> getOrders() {
+        return orderRepository.findAll().stream().map(orderMapper::toDto).collect(Collectors.toList());
     }
 
     @Transactional
-    public void addOrderProduct(Order order) {
-        orderRepository.save(order);
+    public void addOrder(OrderDto orderDto) {
+        orderRepository.save(orderMapper.toEntity(orderDto));
     }
 
     @Transactional
@@ -32,8 +37,8 @@ public class OrderService {
         orderRepository.deleteAll();
     }
 
-    public Order getOrderProduct(Integer id) {
-        return orderRepository.findById(id).orElseThrow(OrderNotFoundException::new);
+    public OrderDto getOrder(Integer id) {
+        return orderRepository.findById(id).map(orderMapper::toDto).orElseThrow(OrderNotFoundException::new);
     }
 
     @Transactional
